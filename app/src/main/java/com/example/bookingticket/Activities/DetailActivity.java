4eef3d0c1c8400bea30768034de4f67aac9cd3a9
DaiNguyenThanh <daiqbn@gmail.com>
2023-12-19 20:53:19 +0700

    detail

--- app/src/main/java/com/example/bookingticket/Activities/DetailActivity.java
+++ app/src/main/java/com/example/bookingticket/Activities/DetailActivity.java
@@ -1,16 +1,162 @@
 package com.example.bookingticket.Activities;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Bitmap;
 import android.os.Bundle;
-
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.example.bookingticket.Domain.Datum;
 import com.example.bookingticket.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView titleTextView;
+    private TextView descriptionTextView;
+
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        imageView = findViewById(R.id.imageView4);
+        titleTextView = findViewById(R.id.textView);
+        descriptionTextView = findViewById(R.id.textView11);
+        int movieId = getIntent().getIntExtra("id", -1);
+        // Initialize Volley RequestQueue
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Make the network request
+        String url = "https://moviesapi.ir/api/v1/movies/"+movieId;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Parse JSON response and update UI
+                        try {
+                            Datum datum = parseMovieDetails(response);
+                            updateUI(datum);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle network errors
+                    }
+                }
+        );
+
+        // Add the request to the RequestQueue
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private Datum parseMovieDetails(JSONObject jsonObject) throws JSONException {
+        int id = jsonObject.getInt("id");
+        String title = jsonObject.getString("title");
+        String poster = jsonObject.getString("poster");
+        String year = jsonObject.getString("year");
+        String rated = jsonObject.getString("rated");
+        String released = jsonObject.getString("released");
+        String runtime = jsonObject.getString("runtime");
+        String director = jsonObject.getString("director");
+        String writer = jsonObject.getString("writer");
+        String actors = jsonObject.getString("actors");
+        String plot = jsonObject.getString("plot");
+        String country = jsonObject.getString("country");
+        String awards = jsonObject.getString("awards");
+        String metascore = jsonObject.getString("metascore");
+        String imdbRating = jsonObject.getString("imdb_rating");
+        String imdbVotes = jsonObject.getString("imdb_votes");
+        String imdbId = jsonObject.getString("imdb_id");
+        String type = jsonObject.getString("type");
+
+        // Parse genres array
+        List<String> genres = new ArrayList<>();
+        JSONArray genresArray = jsonObject.getJSONArray("genres");
+        for (int i = 0; i < genresArray.length(); i++) {
+            genres.add(genresArray.getString(i));
+        }
+
+        // Parse images array
+        List<String> images = new ArrayList<>();
+        JSONArray imagesArray = jsonObject.getJSONArray("images");
+        for (int i = 0; i < imagesArray.length(); i++) {
+            images.add(imagesArray.getString(i));
+        }
+
+        // Create and return Datum object
+        return new Datum(id, title, poster, year, rated, released, runtime, director,
+                writer, actors, plot, country, awards, metascore, imdbRating, imdbVotes,
+                imdbId, type, genres, images);
+    }
+
+    private void updateUI(Datum datum) {
+        // Use a library like Volley's ImageLoader to load the image into ImageView
+        ImageLoader imageLoader = new ImageLoader(requestQueue, new ImageLoader.ImageCache() {
+            @Override
+            public Bitmap getBitmap(String url) {
+                return null;
+            }
+
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                // Optional: Implement caching if needed
+            }
+        });
+
+//        ImageLoader.ImageListener imageListener = ImageLoader.getImageListener(
+//                imageView,
+//                R.drawable.placeholder_image, // Placeholder image resource
+//                R.drawable.error_image); // Error image resource
+//
+//        imageLoader.get("URL_FOR_YOUR_IMAGE", imageListener);
+
+        // Set title and description
+        titleTextView.setText(datum.getTitle());
+        descriptionTextView.setText(datum.getPlot());
+
+        List<String> images = datum.getImages();
+        if (images != null && !images.isEmpty()) {
+            // Assuming you have an ImageView for each additional image
+            ImageView additionalImageView1 = findViewById(R.id.imageView2);
+            ImageView additionalImageView2 = findViewById(R.id.imageView3);
+            Glide.with(this)
+                    .load(images.get(2))  // Assuming getPoster() returns the image URL
+                    .into(imageView);
+            // Load the first additional image
+            Glide.with(this)
+                    .load(images.get(0))
+                    .into(additionalImageView1);
+
+            // Load the second additional image
+            Glide.with(this)
+                    .load(images.get(1))
+                    .into(additionalImageView2);
+
+            // Add more ImageViews and load more images as needed
+        }
     }
 }
+
--- app/src/main/java/com/example/bookingticket/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/bookingticket/Activities/RegisterActivity.java
@@ -1,23 +1,61 @@
 package com.example.bookingticket.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.bookingticket.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
+public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText fullNameEditText;
+//    private EditText birthdayEditText;
+//    private EditText genderEditText;
     private TextView btnLoginView;
+    private AppCompatButton btnSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        btnSignUp=findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+                FirebaseUser user = mAuth.getCurrentUser();
+                String fullName = fullNameEditText.getText().toString();
+
+                addUserInfoToDatabase(user.getUid(), fullName);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
         btnLoginView=findViewById(R.id.btnLoginView);
         btnLoginView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +66,67 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private void signUp() {
+        emailEditText=findViewById(R.id.edtEmail);
+        passwordEditText=findViewById(R.id.edtPassword);
+        fullNameEditText=findViewById(R.id.edtUsername);
+
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String fullName = fullNameEditText.getText().toString();
+//        String birthday = birthdayEditText.getText().toString();
+//        String gender = genderEditText.getText().toString();
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign up success, update UI
+                            Log.d("SignUpActivity", "createUserWithEmail:success");
+
+                            // Get the newly created user
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            // Add additional user information to Firestore or Realtime Database
+                            if (user != null) {
+                                addUserInfoToDatabase(user.getUid(), fullName);
+                            }
+
+                            // Navigate to the main activity or perform other actions
+                        } else {
+                            // If sign up fails, display a message to the user.
+                            Log.w("SignUpActivity", "createUserWithEmail:failure", task.getException());
+                            // Handle errors here
+                        }
+                    }
+                });
+
+
+    }
+    private void addUserInfoToDatabase(String userId, String fullName) {
+        // Example for Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("fullName", fullName);
+//        user.put("birthday", birthday);
+//        user.put("gender", gender);
+
+        db.collection("users").document(userId)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("SignUpActivity", "User information added to Firestore successfully");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("SignUpActivity", "Error adding user information to Firestore", e);
+                    }
+                });
     }
 }
